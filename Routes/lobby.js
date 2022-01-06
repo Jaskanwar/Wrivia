8c1c8cbe387eeed7f67948aa5f3a2e959b2b1fc2
jaskanwar <jaskanwar.singh1@gmail.com>
2022-01-06 17:46:09 -0500

    Basic Pusher setup

--- Routes/lobby.js
+++ Routes/lobby.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Crypto = require('crypto');
 const gameData = require('../Models/gameData');
+const pusher = require('../config/pusher')
 
 
 router.post('/create', async(req, res)=>{
     try {
         let lobbyId = Crypto.randomBytes(6).toString('hex').slice(0, 6);
         const lobby = await gameData.create({lobbyId: lobbyId});
+        pusher.trigger('Wrivia', 'test',{
+            message: "hello world"
+        });
         res.status(200).send({"lobbyId": lobbyId});
     } catch (error) {
         console.log(error);
