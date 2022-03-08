5c26c7f78d9a430ab87cd41a901753999115df92
Tanreet Dhaliwal <tanreet.dhaliwal@gmail.com>
2022-03-08 16:42:38 -0500

    updated lobby styling

--- wrivia-frontend/screens/Lobby.js
+++ wrivia-frontend/screens/Lobby.js
@@ -2,29 +2,39 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Button } from "react-native-elements";
-import { StoreContext } from "../utils/store"
-const axios = require('axios');
+import { StoreContext } from "../utils/store";
+const axios = require("axios");
 
-export default function Lobby({navigation}) {
+export default function Lobby({ navigation }) {
   const baseUrl = "https://wrivia-backend.herokuapp.com/";
-  const {lobbyId: [lobbyID, setLobbyId]} = React.useContext(StoreContext);
-  const {isHost: [isHost, setIsHost]} = React.useContext(StoreContext);
-  function createGame(){
-    axios.post(baseUrl+'api/lobby/create').then((res)=>{
-      setIsHost(true)
-      setLobbyId(res.data.lobbyId)
-      navigation.navigate("EnterName")
-    }).catch((err)=>{
-      console.log(err);
-    });
+  const {
+    lobbyId: [lobbyID, setLobbyId],
+  } = React.useContext(StoreContext);
+  const {
+    isHost: [isHost, setIsHost],
+  } = React.useContext(StoreContext);
+  function createGame() {
+    axios
+      .post(baseUrl + "api/lobby/create")
+      .then((res) => {
+        setIsHost(true);
+        setLobbyId(res.data.lobbyId);
+        navigation.navigate("EnterName");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   return (
     <View style={styles.container}>
       <Text
         style={{
           color: "white",
-          fontSize: 30,
+          fontSize: 40,
           fontWeight: "bold",
+          marginHorizontal: 40,
+          marginTop: -100,
+          textAlign: "center",
         }}
       >
         WELCOME TO WRIVIA
@@ -32,26 +42,30 @@ export default function Lobby({navigation}) {
       <Button
         title={"Start New Game"}
         containerStyle={{
-          width: 200,
+          width: 300,
           marginHorizontal: 50,
+          paddingTop: 60,
           marginVertical: 10,
         }}
         buttonStyle={{
           backgroundColor: "#FF4F63",
         }}
-        onPress={()=> createGame()}
+        titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+        onPress={() => createGame()}
       />
       <Button
         title={"Join An Existing Game"}
         containerStyle={{
-          width: 200,
+          width: 300,
+          paddingTop: 20,
           marginHorizontal: 50,
           marginVertical: 10,
         }}
         buttonStyle={{
           backgroundColor: "#49B5FF",
         }}
-        onPress={()=> navigation.navigate("JoinGame")}
+        titleStyle={{ fontWeight: "bold", fontSize: 18 }}
+        onPress={() => navigation.navigate("JoinGame")}
       />
     </View>
   );
